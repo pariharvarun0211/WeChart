663275df6b6d6a7cd74831d4d1b6b06f217351f5
ejalba <ealba@unomaha.edu>
2017-09-25 21:32:03 -0500

    Update studentHome.blade.php

--- resources/views/student/studentHome.blade.php
+++ resources/views/student/studentHome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h3 style="text-align: center"><img src="logos\LogoStudent.png" width="4%"> Student Dashboard <img src="logos\logostudent.png" width="4%"></h3>
+        <h3 style="text-align: center"><img src="logos\LogoStudent.png" width="4%"> Student Dashboard <img src="logos\LogoStudent.png" width="4%"></h3>
     </div>
     <div style="text-align: center" class="alert alert-info">Work in progress. Please visit after sometime.</div>
 </div>
