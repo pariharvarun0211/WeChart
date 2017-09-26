afcb4a1152666a831b261631741bf6c0fe78367d
ejalba <ealba@unomaha.edu>
2017-09-25 21:32:38 -0500

    Update instructorHome.blade.php

--- resources/views/instructor/instructorHome.blade.php
+++ resources/views/instructor/instructorHome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h3 style="text-align: center"><img src="logos\logoinstructor.png" width="4%"> Instructor Dashboard <img src="logos\logoinstructor.png" width="4%"></h3>
+        <h3 style="text-align: center"><img src="logos\LogoInstructor.png" width="4%"> Instructor Dashboard <img src="logos\LogoInstructor.png" width="4%"></h3>
     </div>
     <div style="text-align: center" class="alert alert-info">Work in progress. Please visit after sometime.</div>
 </div>
