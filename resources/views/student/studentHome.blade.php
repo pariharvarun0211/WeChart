81ee6b24f3749fbf55036b558650d5cfc3b64745
Aditya Chouhan <achouhan@unomaha.edu>
2017-10-08 20:25:49 -0500

    Changed caption

--- resources/views/student/studentHome.blade.php
+++ resources/views/student/studentHome.blade.php
@@ -56,7 +56,7 @@
                                                                 <td><p id="patientWeight">{{$patient->weight}}</p></td>
                                                                 <td><p id="visitDate">{{$patient->visit_date}}</p></td>
                                                                 <td style="text-align: left">
-                                                                    <a class="btn btn-primary" id="edit"> Edit</a>
+                                                                    <a class="btn btn-primary" id="edit">View & Edit</a>
                                                                     <a class="btn btn-danger" id="delete"> Delete</a>
                                                                 </td>
                                                             </tr>
