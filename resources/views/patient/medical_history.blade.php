3c87b3a8ab0754d8a318b4e64247633408e5e755
Aditya Chouhan <achouhan@unomaha.edu>
2017-10-24 10:44:14 -0500

    Aditya adding styles on select2

--- resources/views/patient/medical_history.blade.php
+++ resources/views/patient/medical_history.blade.php
@@ -304,6 +304,10 @@
     </div>
 
      {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
      <script>
          $('#search_diagnosis_personal_history').select2({
