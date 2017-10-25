bb19edc4ce7320ce4a85cffa1c62d776f9954bcf
Aditya Chouhan <achouhan@unomaha.edu>
2017-10-25 09:04:05 -0500

    Aditya vital sign collpasible

--- resources/views/patient/vital_signs_header.blade.php
+++ resources/views/patient/vital_signs_header.blade.php
@@ -3,6 +3,9 @@
 <!--This is a container for vital signs header -->
 <div class="container-fluid" style="padding-top: 0;margin-top:0; padding-bottom: 0;margin-bottom: 0;">
     <div class="row" >
+     <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">
+        <i class="fa fa-window-minimize" aria-hidden="true"></i>
+     </button>
         <div class="panel-heading" style="padding-left: 0">
             <a href="{{url('/StudentHome')}}" class="btn btn-success" style="float: left">
                 <i class="fa fa-arrow-circle-left" aria-hidden="true"></i>
@@ -10,7 +13,7 @@
             </a>
             <h3 align="center" style="margin-top: 0;"><b>Patient Active Record</b></h3>
         </div>
-        <div class="panel-body" style="margin-bottom: 0;padding-bottom: 0;background-color: #FFFAF0;margin-top: 0;padding-top: 0">
+        <div id="demo" class="panel-body collapse" style="margin-bottom: 0;padding-bottom: 0;background-color: #FFFAF0;margin-top: 0;padding-top: 0">
             <table class="table" style=" margin-top: 0;padding-top: 0;margin-bottom: 0;padding-bottom: 0">
                 <!--This is the first row in the vital signs panel -->
                 <tr style=" margin-top: 0;padding-top: 0;">
