8e632e2b213360927b335c8ce9a6db822d155a74
Aditya Chouhan <achouhan@unomaha.edu>
2017-11-04 10:44:31 -0500

    Aditya jquery on not authorized page

--- resources/views/auth/not_authorized.blade.php
+++ resources/views/auth/not_authorized.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
     <div class="row" style="padding-left: 5%">
         <div class="col-md-12">
             <div class="alert alert-info" align="middle">
