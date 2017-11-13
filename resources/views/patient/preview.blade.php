ef29118b6c18409ebe08412653e11a9434208df2
Aditya Chouhan <achouhan@unomaha.edu>
2017-11-13 16:28:44 -0600

    Aditya Chouhan trying collapse on preview page

--- resources/views/patient/preview.blade.php
+++ resources/views/patient/preview.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -147,9 +150,9 @@
     {{--HPI--}}
     <div class="panel panel-default">
         <div class="panel-heading" style="background-color: lightblue">
-            HPI
+           <a data-toggle="collapse" href="#collapse1">HPI</a>
         </div>
-        <div class="panel-body">
+        <div class="panel-body" id="collapse1" class="panel-collapse collapse">
             @if(!count($HPI)>0)
                 <textarea id="HPI" name="HPI" rows="6" cols="50" >
                                         </textarea>
@@ -499,7 +502,5 @@
         </div>
     </div>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </body>
 </html>
