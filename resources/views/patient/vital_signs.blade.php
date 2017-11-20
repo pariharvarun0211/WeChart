a02b0bc7a0254881bf65b83bca3096904ff6b602
Aditya Chouhan <achouhan@unomaha.edu>
2017-11-20 15:00:42 -0600

    Vital signs delete button- removed extra code

--- resources/views/patient/vital_signs.blade.php
+++ resources/views/patient/vital_signs.blade.php
@@ -106,8 +106,7 @@
                                                  <input id="patient_id" name="patient_id" type="hidden" value="{{ $patient->patient_id }}">
                                                  <input type=hidden id="user_id" name="user_id" value="{{ Auth::user()->id }}">
                                                  <button name="delbutton" class="btn btn-danger btn-delete btn-sm" id="delete_vital_signs">Delete</button>
-                                                 {{ Form::close() }}
-                                                 {{ $vs->timestamp }}
+                                                 {{ Form::close() }}                                               
                                              </td>
                                         </tr>
                                     @endforeach
